1c16227738f58246ba785c8c41567832ae3d1ff7
YuJeong <qordbwjd96@gmail.com>
2024-08-05 23:47:24 +0900

    [feat] Button UI Component 추가 (#1)

---
+++ PLUV/Transfer/Entity/MusicPlatform.swift
@@ -0,0 +1,40 @@
+//
+//  MusicPlatform.swift
+//  PLUV
+//
+//  Created by 백유정 on 7/31/24.
+//
+
+import Foundation
+
+enum MusicPlatform: Int, CaseIterable {
+    case AppleMusic
+    case Spotify
+    case YoutubeMusic
+    
+    var name: String {
+        switch self {
+        case .AppleMusic:
+            "애플뮤직"
+        case .Spotify:
+            "스포티파이"
+        case .YoutubeMusic:
+            "유튜브뮤직"
+        }
+    }
+    
+    var icon: String {
+        switch self {
+        case .AppleMusic:
+            "applemusic_icon"
+        case .Spotify:
+            "spotify_icon"
+        case .YoutubeMusic:
+            "youtubemusic_icon"
+        }
+    }
+    
+    var iconSelect: String {
+        icon + "_select"
+    }
+}
