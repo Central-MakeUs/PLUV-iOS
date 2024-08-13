0c4bebe5869732f3b1c7f17d95e06d8a058e31dc
YuJeong <qordbwjd96@gmail.com>
2024-08-14 03:42:46 +0900

    [feat] UIColor 추가 (#4)

--- PLUV/Extension/UIColor+Extension.swift
+++ PLUV/Extension/UIColor+Extension.swift
@@ -9,9 +9,12 @@ import UIKit
 
 extension UIColor {
     static let mainPurple = UIColor(red: 158/256, green: 34/256, blue: 255/256, alpha: 1)
+    static let subBlue = UIColor(red: 46/256, green: 129/256, blue: 255/256, alpha: 1)
     static let shadow = UIColor(red: 42/256, green: 40/256, blue: 50/256, alpha: 0.04)
     static let gray200 = UIColor(white: 242/256, alpha: 1)
     static let gray300 = UIColor(white: 222/256, alpha: 1)
     static let gray400 = UIColor(white: 177/256, alpha: 1)
+    static let gray500 = UIColor(white: 135/256, alpha: 1)
+    static let gray600 = UIColor(white: 92/256, alpha: 1)
     static let gray800 = UIColor(white: 30/256, alpha: 1)
 }
