235332724025a6c8721fd08096cfa1252081f789
YuJeong <qordbwjd96@gmail.com>
2024-08-20 11:48:05 +0900

    [feat] MusicPlatform YoutubeMusic case 제거

--- PLUV/Platform/State/MusicPlatform.swift
+++ PLUV/Platform/State/MusicPlatform.swift
@@ -10,7 +10,6 @@ import Foundation
 enum MusicPlatform: Int, CaseIterable, Codable {
     case AppleMusic
     case Spotify
-    case YoutubeMusic
     
     var name: String {
         switch self {
@@ -18,8 +17,6 @@ enum MusicPlatform: Int, CaseIterable, Codable {
             "애플뮤직"
         case .Spotify:
             "스포티파이"
-        case .YoutubeMusic:
-            "유튜브뮤직"
         }
     }
     
@@ -29,8 +26,6 @@ enum MusicPlatform: Int, CaseIterable, Codable {
             "applemusic_icon"
         case .Spotify:
             "spotify_icon"
-        case .YoutubeMusic:
-            "youtubemusic_icon"
         }
     }
     
@@ -38,3 +33,37 @@ enum MusicPlatform: Int, CaseIterable, Codable {
         icon + "_select"
     }
 }
+
+/*
+ enum MusicPlatform: Int, CaseIterable, Codable {
+     case AppleMusic
+     case Spotify
+     case YoutubeMusic
+     
+     var name: String {
+         switch self {
+         case .AppleMusic:
+             "애플뮤직"
+         case .Spotify:
+             "스포티파이"
+         case .YoutubeMusic:
+             "유튜브뮤직"
+         }
+     }
+     
+     var icon: String {
+         switch self {
+         case .AppleMusic:
+             "applemusic_icon"
+         case .Spotify:
+             "spotify_icon"
+         case .YoutubeMusic:
+             "youtubemusic_icon"
+         }
+     }
+     
+     var iconSelect: String {
+         icon + "_select"
+     }
+ }
+ */
