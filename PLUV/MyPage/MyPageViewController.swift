54c89bc8f29ea3cb769865e50ac581d2884943fc
YuJeong <qordbwjd96@gmail.com>
2024-08-30 04:36:08 +0900

    [feat] MyPageVC 추가 (#38)

---
+++ PLUV/MyPage/MyPageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MyPageViewController.swift
+//  PLUV
+//
+//  Created by 백유정 on 8/30/24.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+    }
+    
+    private func setUI() {
+        self.view.backgroundColor = .white
+        self.navigationItem.setHidesBackButton(true, animated: false)
+    }
+}
