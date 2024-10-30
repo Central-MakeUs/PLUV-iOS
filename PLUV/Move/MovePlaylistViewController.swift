b44e7ddafc1229ee1a28c082790ab5fe9aabb6f8
YuJeong <qordbwjd96@gmail.com>
2024-10-30 21:06:03 +0900

    [feat] 권한 관련 개발 주석

--- PLUV/Move/MovePlaylistViewController.swift
+++ PLUV/Move/MovePlaylistViewController.swift
@@ -232,6 +232,7 @@ class MovePlaylistViewController: UIViewController {
     
     private func searchAPI() {
         if sourcePlatform == .AppleMusic && destinationPlatform == .Spotify {
+            /// 권한이 부여된 경우에만 넘겨야함!!!
             Task {
                 await self.searchAppleToSpotifyAPI(musics: self.viewModel.musicItems)
             }
